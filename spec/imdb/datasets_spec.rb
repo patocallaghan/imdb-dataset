adabae8b55af7ba343ce1e53091e7005889c4913
Pat O'Callaghan <patocallaghan@gmail.com>
2017-09-27 23:58:59 +0100

    Improved specs and small refactor of client

--- spec/imdb/datasets_spec.rb
+++ spec/imdb/datasets_spec.rb
@@ -6,6 +6,35 @@ RSpec.describe Imdb::Datasets do
   end
 
   it "#fetch" do
-    expect(subject.fetch).to eq(true)
+    expect(Aws::S3::Client).to receive(:new).and_call_original
+    expect_any_instance_of(Aws::S3::Client).to receive(:get_object).with(hash_including(
+      bucket: "imdb-datasets",
+      request_payer: "requester"
+    )).exactly(6).times
+    expect_any_instance_of(Aws::S3::Client).to receive(:get_object).with(hash_including(
+      response_target: "imdb_datasets/title.basics.tsv.gz",
+      key: "documents/v1/current/title.basics.tsv.gz",
+    )).at_most(:once)
+    expect_any_instance_of(Aws::S3::Client).to receive(:get_object).with(hash_including(
+      response_target: "imdb_datasets/title.crew.tsv.gz",
+      key: "documents/v1/current/title.crew.tsv.gz",
+    )).at_most(:once)
+    expect_any_instance_of(Aws::S3::Client).to receive(:get_object).with(hash_including(
+      response_target: "imdb_datasets/title.episode.tsv.gz",
+      key: "documents/v1/current/title.episode.tsv.gz",
+    )).at_most(:once)
+    expect_any_instance_of(Aws::S3::Client).to receive(:get_object).with(hash_including(
+      response_target: "imdb_datasets/title.principals.tsv.gz",
+      key: "documents/v1/current/title.principals.tsv.gz",
+    )).at_most(:once)
+    expect_any_instance_of(Aws::S3::Client).to receive(:get_object).with(hash_including(
+      response_target: "imdb_datasets/title.ratings.tsv.gz",
+      key: "documents/v1/current/title.ratings.tsv.gz",
+    )).at_most(:once)
+    expect_any_instance_of(Aws::S3::Client).to receive(:get_object).with(hash_including(
+      response_target: "imdb_datasets/name.basics.tsv.gz",
+      key: "documents/v1/current/name.basics.tsv.gz",
+    )).at_most(:once)
+    subject.fetch
   end
 end
